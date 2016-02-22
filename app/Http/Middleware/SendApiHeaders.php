2a5a8bcaa20d2325578a51d1d598aee954fcab14
frnkly <frank@frnk.ca>
2016-02-22 16:08:12 -0500

    Updated CORS headers

--- app/Http/Middleware/SendApiHeaders.php
+++ app/Http/Middleware/SendApiHeaders.php
@@ -17,23 +17,25 @@ class SendApiHeaders
     {
         $response = $next($request);
 
-        // Set the "Access-Control-Allow-Origin" header.
-        if (strlen($request->header('Origin'))) {
-            $response->header('Access-Control-Allow-Origin', $request->header('Origin'), true);
-        }
+        // NOTE: These headers are set by the server in /public/web.config
 
-        // Respond to pre-flighted requests.
-        if ($request->method() == 'OPTIONS')
-        {
-            // Allow the requested method.
-            if (in_array($request->header('Access-Control-Request-Method'), ['POST', 'PUT', 'PATCH', 'DELETE'])) {
-                $response->header('Access-Control-Allow-Methods', $request->header('Access-Control-Request-Method'), true);
-            }
-            // Allow requested header.
-            if ($request->header('Access-Control-Request-Headers')) {
-                $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'), true);
-            }
-        }
+        // // Set the "Access-Control-Allow-Origin" header.
+        // if (strlen($request->header('Origin'))) {
+        //     $response->header('Access-Control-Allow-Origin', $request->header('Origin'), true);
+        // }
+        //
+        // // Respond to pre-flighted requests.
+        // if ($request->method() == 'OPTIONS')
+        // {
+        //     // Allow the requested method.
+        //     if (in_array($request->header('Access-Control-Request-Method'), ['POST', 'PUT', 'PATCH', 'DELETE'])) {
+        //         $response->header('Access-Control-Allow-Methods', $request->header('Access-Control-Request-Method'), true);
+        //     }
+        //     // Allow requested header.
+        //     if ($request->header('Access-Control-Request-Headers')) {
+        //         $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'), true);
+        //     }
+        // }
 
         return $response;
     }
